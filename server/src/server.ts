c1f4202e638b94394484130b915efa45d5e9ae8c
Pedro Henrique da Silva Pacheco <ppachecobr17@gmail.com>
2022-09-16 11:53:57 -0300

    uptade backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,33 @@
-import express from "express"
+import express from "express";
+import { PrismaClient } from "@prisma/client";
 
-const app = express()
+const app = express();
+const prisma = new PrismaClient();
 
-app.get('/games', (req, res) => {
-    return res.json()
-})
+app.get("/games", async (req, res) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          Ads: true,
+        },
+      },
+    },
+  });
 
-app.post('/ads', (req, res) => {
-    return res.json()
-})
+  return res.json(games);
+});
 
-app.get('/games/:id/ads', (req, res) => {
-    return res.json()
-})
+app.post("/ads", (req, res) => {
+  return res.json();
+});
 
-app.get('/ads/:id/discord', (req, res) => {
-    return res.json()
-})
+app.get("/games/:id/ads", (req, res) => {
+  return res.json();
+});
 
-app.listen(3333)
+app.get("/ads/:id/discord", (req, res) => {
+  return res.json();
+});
+
+app.listen(3333);
